9e9c1725671c579569908f73017b4ea0c87a0d3b
Sai Aathish Karthik <saiaathishk@gmail.com>
2026-08-16 16:33:17 -0500

    chore: prepare AudioShield for submission

--- extension/src/ml/detector/mock-inference.ts
+++
@@ -1,7 +0,0 @@
-import type { DetectorInference, AudioWindow, InferenceLabel } from "./types";
-
-export type MockInference = readonly InferenceLabel[] | ((window: AudioWindow) => readonly InferenceLabel[] | Promise<readonly InferenceLabel[]>);
-export class MockDetectorInference implements DetectorInference {
-  constructor(private readonly result: MockInference) {}
-  infer(window: AudioWindow): Promise<readonly InferenceLabel[]> { return Promise.resolve(typeof this.result === "function" ? this.result(window) : this.result); }
-}
